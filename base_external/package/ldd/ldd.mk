355a10ea4ca17a35b1ed81a98467363169f283e0
Soha18 <sohasaleh@msn.com>
2025-03-16 05:30:13 -0600

    Final commit for assignment 7

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,22 +1,21 @@
-################################################################################
+
+##############################################################
 #
-# ldd package
+# LDD
 #
-################################################################################
+##############################################################
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+LDD_VERSION  = '49df5f93ed66da823f213aecb1c717144ed6b513'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-Soha18.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = NO
 
-LDD_VERSION = custom
-LDD_SITE = $(TOPDIR)/../ldd3
-LDD_SITE_METHOD = local
-
-define LDD_BUILD_CMDS
-    $(MAKE) -C $(@D)/misc-modules ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) modules
-    $(MAKE) -C $(@D)/scull ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) modules
-endef
-
-define LDD_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 644 $(@D)/misc-modules/*.ko $(TARGET_DIR)/lib/modules/
-    $(INSTALL) -D -m 644 $(@D)/scull/*.ko $(TARGET_DIR)/lib/modules/
-endef
+LDD_MODULE_SUBDIRS =  misc-modules
+LDD_MODULE_SUBDIRS +=  scull
 
 $(eval $(generic-package))
+$(eval $(kernel-module))
 
