f13f93e139a63137552417f60ed8a416aad22102
Soha18 <sohasaleh@msn.com>
2025-03-18 04:03:45 -0600

    modify refrence to assignment3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,6 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = '5f2b01fad0d5503d20f6d65ac2610d1d6f6bf300'
+#'5f2b01fad0d5503d20f6d65ac2610d1d6f6bf300'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
